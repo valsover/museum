aebaa6408fdd083345674022fe4b5568001c31de
valsover <vsmakarova.wd@gmail.com>
2021-09-27 18:55:35 +0300

    feat: add gallery section + fix tickets section according to the new layout

--- museum/index.js
+++ museum/index.js
@@ -48,6 +48,105 @@ for (let i = 0; i < cardsArr.length; i++) {
   class="cards__description--small">Google Street Panorama View</span></p></a></div>`;
 }
 
+//Filling Gallery block with imgs
+// function galleryFill() {
+//   const pics = document.getElementById("pics");
+//   const img = document.createElement('img');
+//   img.classList.add('gallery-img')
+//   img.src = `assets/img/galery/galery1.jpg`;
+//   img.alt = `galery1`;
+//   pics.append(img);
+// }
+
+const picsArr = [
+  {
+    src: "assets/img/gallery/galery1.jpg",
+    alt: "Psyche Revived by Cupid's Kiss",
+    title: "Antonio Canova: Psyche Revived by Cupid's Kiss, 1757 – 1822, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery2.jpg",
+    alt: "Artemis",
+    title: "Leochares: Diana of Versailles, C1st - 2nd A.D, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery3.jpg",
+    alt: "Dying Slave",
+    title: "Michelangelo: Dying Slave for the Tomb of Julius II., c. 1513-15, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery4.jpg",
+    alt: "Nike of Samothrace",
+    title: "AWinged Victory of Samothrace, circa 220-185 BC, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery5.jpg",
+    alt: "Venus de Milo",
+    title: "Alexandros of Antioch: Venus de Milo, between 150 and 125 BC, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery6.jpg",
+    alt: "The Virgin and Child with Saint Anne",
+    title: "Leonardo da Vinci: The Virgin and Child with Saint Anne, c. 1503, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery7.jpg",
+    alt: "Mona Lisa",
+    title: "Leonardo da Vinci: Mona Lisa or Giocondo, c. 1503–1506, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery8.jpg",
+    alt: "Mollien",
+    title: "Hector Lefuel: Louvre - grand escalier Mollien"
+  },
+  {
+    src: "assets/img/gallery/galery9.jpg",
+    alt: "Venus of Arles",
+    title: "Venus de Milo, 1st century BC, Paris, Louvre (It may be a copy of the Aphrodite of Thespiae by Praxiteles)"
+  },
+  {
+    src: "assets/img/gallery/galery10.jpg",
+    alt: "Louvr Viscontie",
+    title: "Louvre: Art de l'Islam Cour Visconti - Kanso Déco Ricciotti, Escalier contemporain, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery11.jpg",
+    alt: "Liberty Leading the People",
+    title: "Eugène Delacroix: Liberty Leading the People, 1830, Paris, Louvre "
+  },
+  {
+    src: "assets/img/gallery/galery12.jpg",
+    alt: "Arria et Paetus",
+    title: "Pierre Lepautre: Arria et Paetus, 1681, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery13.jpg",
+    alt: "Ermafrodito borghese",
+    title: "Materazzo del bernini: Ermafrodito borghese, 2nd century AD, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery14.jpg",
+    alt: "La Belle Ferronnière",
+    title: "Leonardo da Vinci or his Milanese circle: La Belle Ferronnière, 1490–1496, Paris, Louvre"
+  },
+  {
+    src: "assets/img/gallery/galery15.jpg",
+    alt: "Louvre Pyramid",
+    title: "Ieoh Ming Pei: Louvre Pyramid, 1988, Paris, Louvre"
+  },
+];
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+shuffle(picsArr);
+
+for (let i = 1; i < picsArr.length; i++) {
+  pics.innerHTML += `<div class="picture">
+    <img src=${picsArr[i].src} alt=${picsArr[i].alt} title=${picsArr[i].title}>
+    </div>`;
+}
+
 //Video control panel
 window.onload = function () {
   player = document.getElementById("player");
