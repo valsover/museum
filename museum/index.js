38b3f5f8e4f65e77c62090f608464013b6af823a
valsover <vsmakarova.wd@gmail.com>
2021-09-30 15:16:31 +0300

    feat: add six html-pages with full-screen Google Street View panoramas embedded

--- museum/index.js
+++ museum/index.js
@@ -9,39 +9,33 @@ for (let i = 0; i < social.length; i++) {
 const cardsContainer = document.getElementById("cards");
 let cardsArr = [
   {
-    link: "https://www.google.com/maps/@48.8606882,2.3356791,3a,90y,328.36h,87.55t/data=!3m7!1e1!3m5!1sAF1QipMZ9YgnArbwEIbTDANeSQYFu9gNqpW_2Sv8FBvY!2e10!3e12!7i4352!8i1815",
     source: "assets/img/royal-palace.jpg",
     name: "Royal Palace"
   },
   {
-    link: "https://www.google.com/maps/@48.8563254,2.3352706,3a,75y,90t/data=!3m7!1e1!3m5!1sAF1QipNRj_CwP4coDMYdCHj6qHeBeJpI2VxU5BUsOX4F!2e10!3e12!7i11000!8i5500",
     source: "assets/img/denon-wing.jpg",
     name: "Denon Wing"
   },
   {
-    link: "https://www.google.com/maps/@48.8601723,2.3395439,3a,88.4y,322.04h,84.25t/data=!3m7!1e1!3m5!1sAF1QipNMZGQuEA-pAUvIG_eP_2f3gWTKZEJ6XLVJ-Pgb!2e10!3e12!7i10240!8i5120",
     source: "assets/img/colonnade-perrault.jpg",
     name: "Colonnade Perrault"
   },
   {
-    link: "https://www.google.com/maps/@48.860183,2.3356156,3a,90y,177.69h,95.61t/data=!3m7!1e1!3m5!1sAF1QipP7uFZnTITRe-7AEVAgHAfqiCL-03gvBHcYWgF3!2e10!3e12!7i5472!8i2736",
     source: "assets/img/greek-hall.jpg",
     name: "Greek Hall"
   },
   {
-    link: "https://www.google.com/maps/@48.8598788,2.3355157,3a,82.2y,7.53h,94t/data=!3m7!1e1!3m5!1sAF1QipO1wkUUbyzpPjj-OR0mR5etZJT-xl-40XK8rDQ3!2e10!3e12!7i10240!8i5120",
     source: "assets/img/mona-lisa.jpg",
     name: "Mona Lisa"
   },
   {
-    link: "https://www.google.com/maps/@48.8563254,2.3352706,3a,90y,21.26h,79.91t/data=!3m7!1e1!3m5!1sAF1QipPpGAowYatVyk3MMGnZAaQkYm2EUk-Dlca06SS5!2e10!3e12!7i11304!8i5652",
     source: "assets/img/night-palace.jpg",
     name: "Night Palace"
   }
 ];
 for (let i = 0; i < cardsArr.length; i++) {
   cardsContainer.innerHTML += `<div><a 
-  href="${cardsArr[i].link}" class="card"><img
+  href="tours/tour${i + 2}.html" target="_blank" class="card"><img
   src="${cardsArr[i].source}" alt="${cardsArr[i].name}" width="440" height="285"><h3
   class="cards__heading forum-font-32 underline-subtitle">${cardsArr[i].name}</h3><p
   class="cards__description roboto-font-22-black">360° Virtual Tour <br><span
