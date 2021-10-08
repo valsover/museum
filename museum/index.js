44ef92f1bb9b52f7e4e4e1ba4a0f44235133b2ea
valsover <vsmakarova.wd@gmail.com>
2021-10-08 14:30:11 +0300

    fix: fixed explore image comparison slider and explore and visiting sections responsiveness

--- museum/index.js
+++ museum/index.js
@@ -434,78 +434,26 @@ swipeDetect(carousel);
 
 
 //EXPLORE IMAGES COMPARE
-function initComparisons() {
-  const exploreOverlay = document.querySelector(".explore__img-overlay");
-  compareImages(exploreOverlay);
+const separator = document.querySelector(".separator");
+const originalPhoto = document.querySelector(".photo-original");
+const filteredPhoto = document.querySelector(".toaster");
+const photoContainer = document.querySelector(".photos");
+let flag = false;
 
-  function compareImages(img) {
-    let slider,
-      clicked = 0,
-      w,
-      h;
-    /*get the width and height of the img element*/
-    w = img.offsetWidth;
-    h = img.offsetHeight;
-    /*set the width of the img element to 50%:*/
-    img.style.width = (w * 0.6) + "px";
-    /*create slider:*/
-    slider = document.createElement("DIV");
-    slider.setAttribute("class", "explore__img-slider");
-    /*insert slider*/
-    img.parentElement.insertBefore(slider, img);
-    /*position the slider in the middle:*/
-    // slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-    slider.style.left = (w * 0.6) - (slider.offsetWidth / 2) + 8 + "px";
-    /*execute a function when the mouse button is pressed:*/
-    slider.addEventListener("mousedown", slideReady);
-    /*and another function when the mouse button is released:*/
-    window.addEventListener("mouseup", slideFinish);
-    /*or touched (for touch screens:*/
-    slider.addEventListener("touchstart", slideReady);
-    /*and released (for touch screens:*/
-    window.addEventListener("touchend", slideFinish);
-    function slideReady(e) {
-      /*the slider is now clicked and ready to move:*/
-      clicked = 1;
-      /*execute a function when the slider is moved:*/
-      window.addEventListener("mousemove", slideMove);
-      window.addEventListener("touchmove", slideMove);
-      /*prevent any other actions that may occur when moving over the image:*/
-      e.preventDefault();
-    }
-    function slideFinish() {
-      /*the slider is no longer clicked:*/
-      clicked = 0;
-    }
-    function slideMove(e) {
-      var pos;
-      /*if the slider is no longer clicked, exit this function:*/
-      if (clicked == 0) return false;
-      /*get the cursor's x position:*/
-      pos = getCursorPos(e);
-      /*prevent the slider from being positioned outside the image:*/
-      if (pos < 0) pos = 0;
-      if (pos > w) pos = w;
-      /*execute a function that will resize the overlay image according to the cursor:*/
-      slide(pos);
-    }
-    function getCursorPos(e) {
-      var a, x = 0;
-      e = e || window.event;
-      /*get the x positions of the image:*/
-      a = img.getBoundingClientRect();
-      /*calculate the cursor's x coordinate, relative to the image:*/
-      x = e.pageX - a.left;
-      /*consider any page scrolling:*/
-      x = x - window.pageXOffset;
-      return x;
-    }
-    function slide(x) {
-      /*resize the image:*/
-      img.style.width = x + "px";
-      /*position the slider:*/
-      slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
-    }
+separator.addEventListener("mousedown", function (evt) {
+  evt.preventDefault();
+  flag = true;
+}, false);
+
+document.addEventListener("mouseup", function () {
+  flag = false;
+}, false);
+
+photoContainer.addEventListener("mousemove", function (evt) {
+  let res = evt.pageX - this.offsetLeft;
+
+  if (flag && res > 0 && res < filteredPhoto.offsetWidth) {
+    separator.style.left = res + "px";
+    originalPhoto.style.width = res + "px";
   }
-}
-initComparisons();
+}, false);
