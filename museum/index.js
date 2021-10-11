f9b91b6b19e38687aa58d8cb283d802789579fba
valsover <vsmakarova.wd@gmail.com>
2021-10-11 19:11:04 +0300

    feat: add fade-up scrolling animation in gallery section

--- museum/index.js
+++ museum/index.js
@@ -184,9 +184,47 @@ for (let i = 0; i < picsCollect.length; i++) {
   }
 }
 
-if (window.scrollY == 4337) {
-  pics.style.backgroundColor = "#fff";
+//FADE-UP SCROLL ANIMATION
+let isScrolling = false;
+window.addEventListener("scroll", throttleScroll, false);
+function throttleScroll(e) {
+  if (isScrolling == false) {
+    window.requestAnimationFrame(function () {
+      scrolling(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
 }
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+function scrolling(e) {
+  for (let i = 0; i < picsCollect.length; i++) {
+    let picsImg = picsCollect[i];
+
+    if (isPartiallyVisible(picsImg)) {
+      picsImg.classList.add("img-show");
+    } else {
+      picsImg.classList.remove("img-show");
+    }
+  }
+}
+
+function isPartiallyVisible(el) {
+  let elementBoundary = el.getBoundingClientRect();
+  let top = elementBoundary.top;
+  let bottom = elementBoundary.bottom;
+  let height = elementBoundary.height;
+  return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+  let elementBoundary = el.getBoundingClientRect();
+  let top = elementBoundary.top;
+  let bottom = elementBoundary.bottom;
+  return ((top >= 0) && (bottom <= window.innerHeight));
+}
+
 
 
 
