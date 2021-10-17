3602d8c4526b22c5df7c1db958bf66bce3ede5a5
valsover <vsmakarova.wd@gmail.com>
2021-10-17 22:16:31 +0300

    feat: add video slides names and links; refactor of video slider script

--- museum/index.js
+++ museum/index.js
@@ -812,25 +812,76 @@ const marker5 = new mapboxgl.Marker({ color: "gray" })
 
 
 //VIDEO CAROUSEL
-const carouselList = document.querySelector(".carousel__list");
+const carouselList = document.querySelector(".carousel__list"); //контейнер со слайдами
+const videoItemName = [
+  { name: "Welcome to the Louvre",
+    link: "zp1BXPX8jcU",},
+  { name: "The Venus de Milo",
+    link: "Vi5D6FKhRmo",},
+  { name: "Mesopotamian collections with A.Thomas",
+    link: "NOhDysLnTvY",},
+  { name: "Exhibition - The Body and the Soul",
+    link: "aWmJ5DgyWPI",},
+  { name: "Little Spring Tales - La ruse du Renard",
+    link: "2OR0OCr6uRE",},
+]; //массив аттрибутов для ссылки на слайде
 
+
+//Функция заполнения контейнера слайдами
 function createSlider() {
   for (let i = 0; i < 5; i++) {
     carouselList.innerHTML += `<li class="video__item">
-                                <p class="carousel-video__wrapper">
+                                <div class="carousel-video__wrapper">
                                   <video
+                                    class="carousel-video"
                                     src="assets/video/video${i}.mp4"
                                     poster="assets/img/video-posters/poster${i}.jpg"
                                   ></video>
-                                </p>
+                                  <p class="video__item-btn"></p>
+                                  <a href="" target="_blank" class="video__item-name"></a>
+                                </div>
                               </li>`;
+  }
+  for (let i = 0; i < videoItemName.length; i++) {
+    document.querySelectorAll(".video__item-name")[i].innerHTML = videoItemName[i].name;
+    document.querySelectorAll(".video__item-name")[i].href = `https://youtu.be/${videoItemName[i].link}`;
   }
 }
 window.onload = createSlider();
 
-const videoItem = document.querySelectorAll(".video__item");
+const videoItem = document.querySelectorAll(".video__item"); //коллекция слайдов
+const carouselVideos = document.querySelectorAll(".carousel-video"); //видео-элемент в слайде
+const videoTags = document.getElementsByTagName("video"); //все видео-элементы в документе
+const videoItemPlayBtn = document.querySelectorAll(".video__item-btn"); //кнопки play на слайде
+
+//Функция проигрывания видео в каждом слайде с остановкой всех остальных видео на странице
+function smallPlay(el, btn) {
+  if (el.paused) {
+    for (let i = 0; i < videoTags.length; i++) {
+      videoTags[i].pause();
+      for (let i = 0; i < videoItemPlayBtn.length; i++) {
+        videoItemPlayBtn[i].style.backgroundImage = "url(assets/svg/play-youtube-btn.svg)";
+      }
+    }
+    el.play();
+    btn.style.backgroundImage = "none";
+  } else {
+    el.pause();
+    btn.style.backgroundImage = "url(assets/svg/play-youtube-btn.svg)";
+  }
+}
+for (let i = 0; i < carouselVideos.length; i++) {
+  carouselVideos[i].addEventListener("click", () => {
+    smallPlay(carouselVideos[i], videoItemPlayBtn[i]);
+  }); //клик по самому видео в каждом слайде
+  videoItemPlayBtn[i].addEventListener("click", () => {
+    smallPlay(carouselVideos[i], videoItemPlayBtn[i]);
+  }); //клик по кнопке "Play" на каждом слайде
+}
+
 let gap, itemWidth;
 
+//Функция установки значения "left" для каждого слайда
 function reposSlideItemLeft() {
   if (window.innerWidth <= 768) {
     gap = Math.round(carouselList.offsetWidth * 0.02747);
@@ -845,96 +896,66 @@ function reposSlideItemLeft() {
     videoItem[i].style.left = `${left}px`;
   }
 }
-
 document.addEventListener("DOMContentLoaded", reposSlideItemLeft);
 window.addEventListener("resize", reposSlideItemLeft);
 
-const nextVideo = document.querySelector(".next_btn");
-const prevVideo = document.querySelector(".prev_btn");
+const nextVideo = document.querySelector(".next_btn"); //кнопка Next (анимация справа-налево)
+const prevVideo = document.querySelector(".prev_btn"); //кнопка Prevew (анимация слева-направо)
 
 nextVideo.addEventListener("click", function () {
-  videoItem.forEach((el) => {
-    switch (el.offsetLeft) {
-      case (gap + itemWidth) * 0:
-        el.style.left = `${(gap + itemWidth) * 4}px`;
-        el.classList.remove("to-right");
-        el.classList.add("to-left");
-        break;
-      case (gap + itemWidth) * 1:
-        el.style.left = `${(gap + itemWidth) * 0}px`;
-        break;
-      case (gap + itemWidth) * 2:
-        el.style.left = `${(gap + itemWidth) * 1}px`;
-        break;
-      case (gap + itemWidth) * 3:
-        el.style.left = `${(gap + itemWidth) * 2}px`;
-        break;
-      case (gap + itemWidth) * 4:
-        el.style.left = `${(gap + itemWidth) * 3}px`;
-        el.classList.remove("to-left");
-        break;
+  for (let i = 0; i < videoItem.length; i++) {
+    if (videoItem[i].offsetLeft == ((gap + itemWidth) * 0)) {
+      videoItem[i].style.left = `${(gap + itemWidth) * (videoItem.length - 1)}px`;
+      videoItem[i].classList.remove("to-right");
+      videoItem[i].classList.add("to-left");
+    } else if (videoItem[i].offsetLeft == ((gap + itemWidth) * (videoItem.length - 1))) {
+      videoItem[i].style.left = `${videoItem[i].offsetLeft - (gap + itemWidth)}px`;
+      videoItem[i].classList.remove("to-left");
+    } else {
+      videoItem[i].style.left = `${videoItem[i].offsetLeft - (gap + itemWidth)}px`;
     }
-  });
-  videoToggleChangesNext();
-});
+  }
+  colorVideoDotsNext();
+}); //перемещение слайдов справа-налево с заполнением буллетов
 
 prevVideo.addEventListener("click", function () {
-  videoItem.forEach((el) => {
-    switch (el.offsetLeft) {
-      case (gap + itemWidth) * 0:
-        el.style.left = `${(gap + itemWidth) * 1}px`;
-        el.classList.remove("to-left");
-        el.classList.remove("to-right");
-        break;
-      case (gap + itemWidth) * 1:
-        el.style.left = `${(gap + itemWidth) * 2}px`;
-        break;
-      case (gap + itemWidth) * 2:
-        el.style.left = `${(gap + itemWidth) * 3}px`;
-        break;
-      case (gap + itemWidth) * 3:
-        el.style.left = `${(gap + itemWidth) * 4}px`;
-        break;
-      case (gap + itemWidth) * 4:
-        el.style.left = `${(gap + itemWidth) * 0}px`;
-        el.classList.remove("to-left");
-        el.classList.add("to-right");
-        break;
-    }
-  });
-  videoToggleChangesPrev();
-});
-
-const videoDots = document.querySelectorAll(".video-dot");
-function videoToggleChangesNext() {
   for (let i = 0; i < videoItem.length; i++) {
-    if (videoItem[i].classList[1] === "to-left" && i <= 3) {
-      videoDots[i + 1].classList.add("slider__dot_default");
-      videoDots[i].classList.remove("slider__dot_default");
-      player.src = `assets/video/video${i + 1}.mp4`;
-      player.poster = `assets/img/video-posters/poster${i + 1}.jpg`;
-    } else if (videoItem[i].classList[1] === "to-left" && i == 4) {
-      videoDots[i].classList.remove("slider__dot_default");
-      videoDots[0].classList.add("slider__dot_default");
-      player.src = `assets/video/video0.mp4`;
-      player.poster = `assets/img/video-posters/poster0.jpg`;
+    if (videoItem[i].offsetLeft == ((gap + itemWidth) * 0)) {
+      videoItem[i].style.left = `${(gap + itemWidth) * 1}px`;
+      videoItem[i].classList.remove("to-left");
+      videoItem[i].classList.remove("to-right");
+    } else if (videoItem[i].offsetLeft == ((gap + itemWidth) * (videoItem.length - 1))) {
+      videoItem[i].style.left = `${(gap + itemWidth) * 0}px`;
+      videoItem[i].classList.remove("to-left");
+      videoItem[i].classList.add("to-right");
+    } else {
+      videoItem[i].style.left = `${videoItem[i].offsetLeft + (gap + itemWidth)}px`;
+    }
+  }
+  colorVideoDotsPrev();
+});//перемещение слайдов слева-направо с заполнением буллетов
+
+const videoDots = document.querySelectorAll(".video-dot"); //коллекция буллетов
+
+//Функции заполнения буллетов при клике на "Next" и "Prevew"
+function colorVideoDotsNext() {
+  for (let i = 0; i < videoItem.length; i++) {
+    videoDots[i].classList.remove("slider__dot_default");
+    if (videoItem[i].offsetLeft == gap + itemWidth) {
+      videoDots[i].classList.add("slider__dot_default");
+      player.src = `assets/video/video${i}.mp4`;
+      player.poster = `assets/img/video-posters/poster${i}.jpg`;
     }
   }
 }
-
-function videoToggleChangesPrev() {
+function colorVideoDotsPrev() {
   for (let i = 0; i < videoItem.length; i++) {
     videoDots[i].classList.remove("slider__dot_default");
-    if (videoItem[i].classList[1] === "to-right" && i <= 3) {
+    if (videoItem[i].classList[1] === "to-right") {
       videoDots[i].classList.add("slider__dot_default");
-      videoDots[i + 1].classList.remove("slider__dot_default");
       player.src = `assets/video/video${i}.mp4`;
       player.poster = `assets/img/video-posters/poster${i}.jpg`;
-    } else if (videoItem[i].classList[1] === "to-right" && i == 4) {
-      videoDots[i].classList.add("slider__dot_default");
-      videoDots[0].classList.remove("slider__dot_default");
-      player.src = `assets/video/video${i}.mp4`;
-      player.poster = `assets/img/video-posters/poster${i}.jpg`;
+      console.log(videoItem[i], videoDots[i])
     }
   }
 }
